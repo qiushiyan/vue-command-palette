b516c4779940663c703ae639ac434c46eaddabf4
qiushi <qiushi.yann@gmail.com>
2022-10-29 22:42:21 -0500

    index off 1

--- src/components/CommandPalette.vue
+++ src/components/CommandPalette.vue
@@ -1,35 +1,48 @@
 <script setup lang = 'ts'>
 import { useMagicKeys, whenever } from '@vueuse/core';
-import { computed, ref } from "vue";
+import { nextTick, ref, watch } from "vue";
 import { useCommandStore } from "../stores/CommandStore";
 
 const commandStore = useCommandStore();
+const searchBar = ref<HTMLInputElement | null>(null)
+const searchBarFocused = ref(false)
 
-const commandFilterText = ref("");
-const commandListFiltered = computed(() => {
-    if (commandFilterText.value === "") {
-        return commandStore.commandList
-    }
-
-    return commandStore.commandList.filter((command) => {
-        return command.title.toLowerCase().includes(commandFilterText.value.toLowerCase())
-    })
-})
-
+const handleInput = (e: Event) => {
+    commandStore.setCommandFilterText((e.target as HTMLInputElement).value)
+}
 
 const keys = useMagicKeys()
 const CtrlShiftP = keys["ctrl+shift+p"]
+
+watch(commandStore, async () => {
+    if (commandStore.commandPaletteVisible && !searchBarFocused.value) {
+        await nextTick()
+        searchBar.value?.focus()
+        searchBarFocused.value = true
+    }
+})
 
 whenever(CtrlShiftP, () => {
     commandStore.toggleCommandPalette()
 })
 
+whenever(keys.down, () => {
+    if (commandStore.commandPaletteVisible) {
+        commandStore.incrementActiveIdx()
+    }
+})
+
+whenever(keys.up, () => {
+    if (commandStore.commandPaletteVisible) {
+        commandStore.decrementActiveIdx()
+    }
+})
 
 </script>
 
 <template>
     <Teleport to="body">
-        <div class="w-6/12 m-auto" v-if="commandStore.showCommandPalette">
+        <div class="w-6/12 m-auto" v-show="commandStore.commandPaletteVisible">
             <h2 class="text-white text-2xl">command palette</h2>
             <div class="relative w-full">
                 <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
@@ -40,13 +53,15 @@ whenever(CtrlShiftP, () => {
                             clip-rule="evenodd"></path>
                     </svg>
                 </div>
-                <input type="text" id="search" v-model="commandFilterText"
+                <input type="text" id="search" :value="commandStore.commandFilterText" @input="handleInput"
+                    ref="searchBar"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Search" required>
             </div>
             <div class="commands">
                 <ul>
-                    <li v-for="command in commandListFiltered" :key="command.id">
+                    <li v-for="(command, idx) in commandStore.filteredCommandList" :key="command.id" class="rounded p-2"
+                        :class="commandStore.activeIdx === idx ? 'is-hovered' : ''">
                         {{ command.title }}</li>
                 </ul>
             </div>
@@ -56,5 +71,7 @@ whenever(CtrlShiftP, () => {
 </template>
 
 <style scoped>
-
+.is-hovered {
+    background-color: #4a5568;
+}
 </style>
--- src/stores/CommandStore.ts
+++ src/stores/CommandStore.ts
@@ -3,7 +3,9 @@ import { defineStore } from "pinia";
 export const useCommandStore = defineStore("CommandPalette", {
   state: () => {
     return {
-      showCommandPalette: false,
+      commandPaletteVisible: false,
+      commandFilterText: "",
+      activeIdx: 0,
       commandList: [
         { id: 1, title: "command 1", alias: [], hotkeys: [] },
         { id: 2, title: "command 2", alias: [], hotkeys: [] },
@@ -12,9 +14,40 @@ export const useCommandStore = defineStore("CommandPalette", {
     };
   },
 
+  getters: {
+    filteredCommandList: (state) => {
+      if (state.commandFilterText === "") {
+        return state.commandList;
+      }
+
+      return state.commandList.filter((command) => {
+        return command.title
+          .toLowerCase()
+          .includes(state.commandFilterText.toLowerCase());
+      });
+    },
+  },
+
   actions: {
+    setCommandFilterText(text: string) {
+      this.commandFilterText = text;
+    },
     toggleCommandPalette() {
-      this.showCommandPalette = !this.showCommandPalette;
+      this.commandPaletteVisible = !this.commandPaletteVisible;
+    },
+    incrementActiveIdx() {
+      if (this.activeIdx >= this.filteredCommandList.length - 1) {
+        this.activeIdx = 0;
+      } else {
+        this.activeIdx++;
+      }
+    },
+    decrementActiveIdx() {
+      if (this.activeIdx <= 0) {
+        this.activeIdx = this.filteredCommandList.length - 1;
+      } else {
+        this.activeIdx--;
+      }
     },
   },
 });
